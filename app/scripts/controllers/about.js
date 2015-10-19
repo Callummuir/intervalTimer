1c9257f9d2ab6d5d9ab48ced3c2de8fa5efd0aa1
Callum Muir <steppinyello@gmail.com>
2015-10-19 16:12:07 +0200

    initial Yeoman framework

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intervalTimerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the intervalTimerApp
+ */
+angular.module('intervalTimerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name intervalTimerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the intervalTimerApp
+ */
+angular.module('intervalTimerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
