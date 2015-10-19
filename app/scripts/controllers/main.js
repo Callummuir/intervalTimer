f335a952bfca218f0e9f194b6a8bcc976f90db54
Callum Muir <steppinyello@gmail.com>
2015-10-19 17:39:50 +0200

    made a basic countdown, will need going over and improving

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,7 @@
 angular.module('intervalTimerApp')
   .controller('MainCtrl', function ($scope) {
     
-
+  	//going to have to remove these from the countdown values as the yare just for input
   	$scope.timee = 0;
   	$scope.interval = 0;
   	$scope.round = 1;
@@ -19,6 +19,50 @@ angular.module('intervalTimerApp')
   		var total = (time + interval) * round;
   		return total;
   	};
+
+  	//Starts the (final)countdown 
+  	//TODO do the countdown better
+  	$scope.startCountdown = function(){
+  		if($scope.timee === 0){
+  			if($scope.interval === 0){
+  				if($scope.round === 0){
+  					console.log("countdown over!");
+  					return;
+  				}else{
+  					countDownRound();
+  				}
+
+  			}else{
+  				countDownInterval();
+  			}
+
+  		}else{
+  			countDownTime();
+  		}
+
+  		$scope.startCountdown();
+
+  	};
+
+  	var countDownTime = function(){
+  		console.log("count time");
+  		$scope.timee -= 1;
+  	};
+
+		var countDownInterval = function(){
+			console.log("count tinterval");
+  		$scope.interval -= 1;
+  	};
+
+
+		var countDownRound = function(){
+			console.log("count round");
+  		$scope.round -= 1;
+  		$scope.timee = 10;
+  		$scope.interval = 10;
+  	};
+
+
 
 
   });
