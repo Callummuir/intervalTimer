1587af5aace7793b369554e5bdc0a21de5336c81
Callum Muir <steppinyello@gmail.com>
2015-10-21 12:46:53 +0200

    rearanged counting variables

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,22 +10,23 @@
 angular.module('intervalTimerApp')
   .controller('MainCtrl', function ($scope) {
     
-  	//going to have to remove these from the countdown values as the yare just for input
-  	$scope.timee = 0;
-  	$scope.interval = 0;
-  	$scope.round = 1;
+  	// These variables store the start values
+  	$scope.roundTime = 0;
+  	$scope.intervalTime = 0;
+  	$scope.roundNumber = 1;
+  	// These varibles are counted on
 
-  	$scope.total = function(time, interval, round){
-  		var total = (time + interval) * round;
-  		return total;
+
+  	$scope.total = function(){
+  		return ($scope.roundTime + $scope.intervalTime) * $scope.roundNumber;
   	};
 
   	//Starts the (final)countdown 
   	//TODO do the countdown better
   	$scope.startCountdown = function(){
-  		if($scope.timee === 0){
-  			if($scope.interval === 0){
-  				if($scope.round === 0){
+  		if($scope.roundTime === 0){
+  			if($scope.intervalTime === 0){
+  				if($scope.roundNumber === 0){
   					console.log("countdown over!");
   					return;
   				}else{
@@ -46,21 +47,23 @@ angular.module('intervalTimerApp')
 
   	var countDownTime = function(){
   		console.log("count time");
-  		$scope.timee -= 1;
+  		$scope.roundTime -= 1;
   	};
 
 		var countDownInterval = function(){
 			console.log("count tinterval");
-  		$scope.interval -= 1;
+  		$scope.intervalTime -= 1;
   	};
 
 
 		var countDownRound = function(){
 			console.log("count round");
-  		$scope.round -= 1;
-  		$scope.timee = 10;
-  		$scope.interval = 10;
+  		$scope.roundNumber -= 1;
+  		$scope.roundTime = 10;
+  		$scope.intervalTime = 10;
   	};
+
+
 
 
 
