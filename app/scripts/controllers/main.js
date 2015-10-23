cbe9d56844ed125b4bd5786733867d07e05816d3
Callum Muir <steppinyello@gmail.com>
2015-10-23 14:35:46 +0200

    now uses real time to count down. Uses milliseconds in the backend as this is what Date.now() uses and is therefore more accurate. Now works when off the tab, but there is a very slight (millisecond) off sometimes as interval is still used  to check the time. Bug now fixed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,13 +20,9 @@ angular.module('intervalTimerApp')
   	var thisRoundNumber = 1;
   	var timePassed = 0;
 
-  	//timer
+  	//Start time
+  	var startTime = null;
   	var countdownTimer;
-
-  	//audio
-  	var pointAudioPlayedInRound = false;
-  	var endAudio = new Audio('audio/endBeep.wav');
-  	var pointAudio = new Audio('audio/partBeep.wav');
 
   	//Show/hide variable for buttons
   	$scope.showStartButton = true;
@@ -35,16 +31,22 @@ angular.module('intervalTimerApp')
   	$scope.showStopButton = false;
   	$scope.showRestartButton = false;
 
+  	//audio
+  	var pointAudioPlayedInRound = false;
+  	var endAudio = new Audio('audio/endBeep.wav');
+  	var pointAudio = new Audio('audio/partBeep.wav');
+
   	$scope.showDisplay = false;
   	$scope.showInput = true;
 
 
   	//Calculates the total amount of time to display
   	$scope.total = function(){
-  	  return (($scope.roundTime + $scope.intervalTime) * $scope.roundNumber) - timePassed;	
+  	  return Math.round((($scope.roundTime / 1000) + ($scope.intervalTime / 1000)) * $scope.roundNumber) - (timePassed / 1000);	
   	};
 
-		///////////// Button functions /////////////  	
+
+  	///////////// Button functions /////////////  	
 
   	//Function when the start button is pressed
   	$scope.startButtonPress = function(){
@@ -58,10 +60,11 @@ angular.module('intervalTimerApp')
   		$scope.showInput = false;
 
   		//Function
-  		thisRoundTime = $scope.roundTime;
-  		thisIntervalTime = $scope.intervalTime;
+  		thisRoundTime = $scope.roundTime * 1000;
+  		thisIntervalTime = $scope.intervalTime * 1000;
   		thisRoundNumber = $scope.roundNumber;
   		timePassed = 0;
+
   		startCountDown();
   	};
 
@@ -79,7 +82,6 @@ angular.module('intervalTimerApp')
 
   		//Function
   		endCountDown();
-  		stopEndAudio();
   		$scope.roundTime = 0;
   		$scope.intervalTime = 0;
   		$scope.roundNumber = 1;
@@ -94,20 +96,16 @@ angular.module('intervalTimerApp')
 
   		//Function
   		endCountDown();
-  		stopEndAudio();
-
   	};
 
   	//Function for reset button is pressed
   	$scope.resetButtonPress = function(){
   		//Change Display
 
-
   		//Functiom
   		$scope.roundTime = 0;
   		$scope.intervalTime = 0;
   		$scope.roundNumber = 1;
-  		timePassed = 0;
   	};
 
   	//function for restarting the countdown after a pause
@@ -120,43 +118,62 @@ angular.module('intervalTimerApp')
   		startCountDown();
   	};
 
+
   	///////////// Count functions /////////////
 
   	//Function for starting counting functionality
   	var startCountDown = function(){
-  		countdownTimer = setInterval(countDown, 1000);
+  		//milliseconds since 
+  		startTime = Date.now();
+
+  		//require interval timer for updating the values
+			countdownTimer = setInterval(findTimePassed, 50);
+  	};
+
+  	//find the real time passed
+  	//Note this may be a bit off as we are counting second and not milliseconds. tempted to change this, but will be easy to fix later
+  	var findTimePassed = function(){
+  		var thisTime = Date.now();
+  		var timePassed = thisTime - startTime;
+  		startTime = thisTime;
+  		countDown(timePassed);
   	};
 
   	//the final countdown function 
-  	var countDown = function(){
-  		if(thisRoundTime === 0){
-  			if(!pointAudioPlayedInRound){
-  				pointAudioPlayedInRound = true;
-  				playPartAudio();
-  			}
-  			if(thisIntervalTime === 0){
-  				if(pointAudioPlayedInRound){
-  					playPartAudio();
-  				}
-  				//Time is indexed to 0, number of times counted indexed to 1
-  				if(thisRoundNumber === 1){
-						playEndAudio();
-  					endCountDown();
-  				}else{
-  					countDownRound();
-  				}
-  			}else{
-  				countDownInterval();
-  			}
-  		}else{
-  			countDownTime();
-  		}
+  	var countDown = function(timePassed){
+  		//counts down one for each millisecond that has passed
+  		for(var i = 0; i < timePassed; i++){
+	  		if(thisRoundTime === 0){
+	  			if(!pointAudioPlayedInRound){
+	  				pointAudioPlayedInRound = true;
+	  				playPartAudio();
+	  			}
+	  			if(thisIntervalTime === 0){
+	  				if(pointAudioPlayedInRound){
+	  					playPartAudio();
+	  				}
+	  				//Time is indexed to 0, number of times counted indexed to 1
+	  				if(thisRoundNumber === 1){
+							playEndAudio();
+	  					endCountDown();
+	  					break;
+	  				}else{
+	  					countDownRound();
+	  				}
+	  			}else{
+	  				countDownInterval();
+	  			}
+	  		}else{
+	  			countDownTime();
+	  		}
+	  	}
   		$scope.$apply();
   	};
 
   	//Function when the countdown is finished
   	var endCountDown = function(){
 			clearInterval(countdownTimer);
+			startTime = null;
   	};
 
   	//function for counting down the value of the time
@@ -175,8 +192,8 @@ angular.module('intervalTimerApp')
   	// Fucntion for counting down the value of the round
 		var countDownRound = function(){
   		thisRoundNumber -= 1;
-  		thisRoundTime = $scope.roundTime - 1;
-  		thisIntervalTime = $scope.intervalTime;
+  		thisRoundTime = $scope.roundTime * 1000;
+  		thisIntervalTime = $scope.intervalTime * 1000;
   		timePassed += 1;
   		pointAudioPlayedInRound = false;
   	};
@@ -192,15 +209,14 @@ angular.module('intervalTimerApp')
 			pointAudio.play();
   	};
 
-
   	///////////// Misc Functions /////////////
 
   	$scope.getThisRoundTime = function(){
-  		return thisRoundTime;
+  		return Math.round(thisRoundTime / 1000);
   	};
 
   	$scope.getThisIntervalTime = function(){
-  		return thisIntervalTime;
+  		return Math.round(thisIntervalTime / 1000);
   	};
 
   	$scope.getThisRoundNumber = function(){
